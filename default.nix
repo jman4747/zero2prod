5c7c71d34044c2a390325fdc49d1ffa75efbea2b
Joshua Turner <joshflyer5tp@gmail.com>
2023-06-25 19:41:14 -0400

    added cargo-watch set up cargo & rustc in nix so lld works

--- default.nix
+++ default.nix
@@ -5,6 +5,15 @@ pkgs.mkShell {
     name="dev-environment";
     buildInputs = [
         pkgs.lld_9
+        pkgs.cargo-watch
+        pkgs.cargo
+        pkgs.rustc
     ];
+
+    shellHook = ''
+        echo "default rust dev environment"
+        echo "run: cargo watch -x check -x test -x run"
+    '';
+
 }
 
