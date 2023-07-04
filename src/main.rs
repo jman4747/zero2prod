3d05d056445a7e9817016f926f1ff543733c3581
Joshua Turner <joshflyer5tp@gmail.com>
2023-07-03 20:41:36 -0400

    split into lib & bin

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(root))
-            .route("/heath", web::get().to(health_check))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn root() -> impl Responder {
-    HttpResponse::ServiceUnavailable()
+    zero2prod::run().await
 }
